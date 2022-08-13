a54ab2f75c6b2dfb98571c8747084c61071632c3
Aram Guzelian <araguzel66@gmail.com>
2022-08-13 06:43:36 -0300

    Creando Backend, conecte MySql + Spring boot

---
+++ PortfolioFullStack/Backend/araguzel66/src/test/java/com/araguzel/araguzel66/AramGuzelianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araguzel.araguzel66;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AramGuzelianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
